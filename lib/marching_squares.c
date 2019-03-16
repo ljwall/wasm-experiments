776cbc4027243a4b87ee947c6692696f6a968f18
liam <liam@magicseaweed.com>
2019-03-16 17:19:11 +0000

    Make smooth

--- lib/marching_squares.c
+++ lib/marching_squares.c
@@ -2,9 +2,11 @@
 
 void marchingSquares(float *f, float cutoff, int m, int n, int hContext) {
 	float *a, *b, *c, *d;
+	float t;
 	unsigned char type;
-	unsigned char types[m * n];
-	int i, j, count = 0;
+	int i, j;
+
+	context2d_beginPath(hContext);
 
 	for (i = 0; i < m - 1; i++) {
 		a = &(f[i*n]);		c = &(a[1]);
@@ -24,70 +26,66 @@ void marchingSquares(float *f, float cutoff, int m, int n, int hContext) {
 			if (*c >= cutoff) type |= 0b0010;
 			if (*d >= cutoff) type |= 0b0001;
 
-			types[i*n + j] = type;
-
 			if (0 < type && type < 0b1111) {
-				count++;
-			}
-
-			a++; b++; c++; d++;
-		}
-	}
-
-	unsigned char t;
-
-	context2d_beginPath(hContext);
-
-	for (i = 0; i < m - 1; i++) {
-		for (j = 0; j < n; j++) {
-			t = types[i*n + j];
-
-			if (0 < t && t < 0b1111) {
-				switch (t) {
+				switch (type) {
 					case 0b0001:
 					case 0b1110:
-						context2d_moveTo(hContext, j + 0.5, i + 1);
-						context2d_lineTo(hContext, j + 1, i + 0.5);
+						t = (cutoff - *b)/(*d - *b);
+						context2d_moveTo(hContext, j + t, i + 1);
+						t = (cutoff - *c)/(*d - *c);
+						context2d_lineTo(hContext, j + 1, i + t);
 						break;
 					case 0b0010:
 					case 0b1101:
-						context2d_moveTo(hContext, j + 0.5, i);
-						context2d_lineTo(hContext, j + 1, i + 0.5);
+						t = (cutoff - *a)/(*c - *a);
+						context2d_moveTo(hContext, j + t, i);
+						t = (cutoff - *c)/(*d - *c);
+						context2d_lineTo(hContext, j + 1, i + t);
 						break;
 					case 0b0011:
 					case 0b1100:
-						context2d_moveTo(hContext, j + 0.5, i);
-						context2d_lineTo(hContext, j + 0.5, i + 1);
+						t = (cutoff - *a)/(*c - *a);
+						context2d_moveTo(hContext, j + t, i);
+						t = (cutoff - *b)/(*d - *b);
+						context2d_lineTo(hContext, j + t, i + 1);
 						break;
 					case 0b0100:
 					case 0b1011:
-						context2d_moveTo(hContext, j, i + 0.5);
-						context2d_lineTo(hContext, j + 0.5, i + 1);
+						t = (cutoff - *a)/(*b - *a);
+						context2d_moveTo(hContext, j, i + t);
+						t = (cutoff - *b)/(*d - *b);
+						context2d_lineTo(hContext, j + t, i + 1);
 						break;
 					case 0b0101:
 					case 0b1010:
-						context2d_moveTo(hContext, j, i + 0.5);
-						context2d_lineTo(hContext, j + 1, i + 0.5);
+						t = (cutoff - *a)/(*b - *a);
+						context2d_moveTo(hContext, j, i + t);
+						t = (cutoff - *c)/(*d - *c);
+						context2d_lineTo(hContext, j + 1, i + t);
 						break;
 					case 0b0110:
 					case 0b1001:
-						context2d_moveTo(hContext, j, i + 0.5);
-						context2d_lineTo(hContext, j + 0.5, i + 1);
-						context2d_moveTo(hContext, j + 0.5, i);
-						context2d_lineTo(hContext, j + 1, i + 0.5);
+						t = (cutoff - *a)/(*b - *a);
+						context2d_moveTo(hContext, j, i + t);
+						t = (cutoff - *b)/(*d - *b);
+						context2d_lineTo(hContext, j + t, i + 1);
+
+						t = (cutoff - *a)/(*c - *a);
+						context2d_moveTo(hContext, j + t, i);
+						t = (cutoff - *c)/(*d - *c);
+						context2d_lineTo(hContext, j + 1, i + t);
 						break;
 					case 0b0111:
 					case 0b1000:
-						context2d_moveTo(hContext, j, i + 0.5);
-						context2d_lineTo(hContext, j + 0.5, i);
+						t = (cutoff - *a)/(*b - *a);
+						context2d_moveTo(hContext, j, i + t);
+						t = (cutoff - *a)/(*c - *a);
+						context2d_lineTo(hContext, j + t, i);
 						break;
 				}
-
-				if (--count == 0) {
-					// Break from both for loops
-					i = m - 1; break;
-				}
 			}
+
+			a++; b++; c++; d++;
 		}
 	}
 
